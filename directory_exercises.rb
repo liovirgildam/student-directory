a847992a27001c7ec10ce55a26e53b280277d616
liovirgilda <liovirgilda.developer@gmail.com>
2022-08-07 16:09:31 +0000

    step8- ex. 7 set default method

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -5,34 +5,39 @@ def input_students
   students = []
   #get the name
   name = gets.chomp
-  puts "Enter country of the student"
-  country = gets.chomp
+  puts "Enter cohort of the student"
+  cohort = gets.chomp 
   #while the name is not empty, repeat this code
   while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, country: country, cohort: :august}
+    #add the student hash to the students array
+    
+    if !cohort.empty?
+      students << {name: name, cohort: cohort.to_sym}
+    else
+      students << {name: name, cohort: :august}
+    end
     puts "Now we have #{students.count} student"
     #get another name from the user
     name = gets.chomp
-    country = gets.chomp
+    cohort = gets.chomp 
   end
   # return another name from the user
   students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(45)
-  puts "--------------------------------".center(45)
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} - #{student[:country]} (#{student[:cohort]} cohort)".center(45)
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(45)
+  puts "Overall, we have #{students.count} great students"
 end
 
 #nothing happens until we call the methods
