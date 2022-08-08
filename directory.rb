d01630bc2d50a8fb0740bf0bd4621567d0bbd40e
liovirgilda <liovirgilda.developer@gmail.com>
2022-08-08 16:06:33 +0000

    Organise code order, replace students with @student variable

--- directory.rb
+++ directory.rb
@@ -3,24 +3,15 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   #get the name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :august}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :august}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
-  end
-end
-
-def interactive_menu
-  
-  loop do
-    
   end
 end
 
@@ -30,10 +21,25 @@ def print_menu
   puts "9. Exit" #9 because we'll be adding more times
 end
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
+end
+
+def print
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
 def show_students
   print_header
-  print(students)
-  print_footer(students)
+  print
+  print_footer
 end
 
 def process(selection)
@@ -49,19 +55,11 @@ def process(selection)
   end
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "--------------------------------"
-end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
-end
-
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
 end
 
 #nothing happens until we call the methods
