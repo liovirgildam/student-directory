eab3ebd4a1d4dda426bdb174da6fcfb8135cfdd3
liovirgilda <liovirgilda.developer@gmail.com>
2022-08-08 15:52:00 +0000

    Create process and print_menu methods

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -13,34 +15,37 @@ def input_students
     #get another name from the user
     name = gets.chomp
   end
-  # return another name from the user
-  students
 end
 
 def interactive_menu
-  students = []
+  
   loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. read the input and save it into a variable
-    selection = gets.chomp
-    #3. do what the user asked
-    case selection
-      when "1"
-        #input the students
-        students = input_students
-      when "2"
-        # show the students
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+    
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #9 because we'll be adding more times
+end
+
+def show_students
+  print_header
+  print(students)
+  print_footer(students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
   end
 end
 
